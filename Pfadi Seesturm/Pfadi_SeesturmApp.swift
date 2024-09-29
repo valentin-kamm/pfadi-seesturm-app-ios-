4bbd45324c75719f470e00399aee5d993a0c8c02
Valentin Kamm <valentinkamm@gmail.com>
2024-09-29 15:46:57 +0200

    Initial Commit

---
+++ Pfadi Seesturm/Pfadi_SeesturmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pfadi_SeesturmApp.swift
+//  Pfadi Seesturm
+//
+//  Created by Valentin Kamm on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pfadi_SeesturmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pfadi SeesturmTests/Pfadi_SeesturmTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pfadi_SeesturmTests.swift
+//  Pfadi SeesturmTests
+//
+//  Created by Valentin Kamm on 29.09.2024.
+//
+
+import Testing
+@testable import Pfadi_Seesturm
+
+struct Pfadi_SeesturmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
